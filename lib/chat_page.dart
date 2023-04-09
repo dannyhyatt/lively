d19fd361b557414797a76ccb4d089a360a113e9e
danny hyatt <daf281@aol.com>
2023-04-09 08:46:51 -0400

    add collection id

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({super.key});
+  String collectionId;
+  ChatPage({super.key, required this.collectionId});
 
   @override
   State<ChatPage> createState() => _ChatPageState();
